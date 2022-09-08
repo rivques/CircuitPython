6891f3dbb269885fb67b8caf2975e7212dddf40e
rivques <38469076+rivques@users.noreply.github.com>
2022-09-08 17:19:18 -0400

    add io

---
+++ internet.py
@@ -0,0 +1,71 @@
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_requests as requests
+from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
+from secrets import secrets
+import adafruit_esp32spi.adafruit_esp32spi_wsgiserver as server
+import neopixel
+from adafruit_wsgi.wsgi_app import WSGIApp
+
+status_light = neopixel.NeoPixel(
+    board.NEOPIXEL, 1, brightness=0.2
+)
+
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+
+socket.set_interface(esp)
+requests.set_socket(socket, esp)
+
+web_app = WSGIApp()
+
+
+@web_app.route("/led_on/<r>/<g>/<b>")
+def led_on(request, r, g, b):  # pylint: disable=unused-argument
+    print("led on!")
+    status_light.fill((int(r), int(g), int(b)))
+    return ("200 OK", [], "led on!")
+
+
+@web_app.route("/led_off")
+def led_off(request):  # pylint: disable=unused-argument
+    print("led off!")
+    status_light.fill(0)
+    return ("200 OK", [], "led off!")
+
+
+# Here we setup our server, passing in our web_app as the application
+server.set_interface(esp)
+wsgiServer = server.WSGIServer(80, application=web_app)
+
+print("open this IP in your browser: ", esp.pretty_ip(esp.ip_address) + "/led_on/255/0/0")
+
+# print(esp.get_time())
+# Start the server
+wsgiServer.start()
+while True:
+    # Our main loop where we have the server poll for incoming requests
+    try:
+        wsgiServer.update_poll()
+        # Could do any other background tasks here, like reading sensors
+    except (ValueError, RuntimeError) as e:
+        print("Failed to update server, restarting ESP32\n", e)
+        esp.reset()
+        continue
