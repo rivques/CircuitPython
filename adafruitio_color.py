abb9ca7d02f9e9e498e0967476a389b07ad75bd4
rivques <38469076+rivques@users.noreply.github.com>
2022-09-10 10:47:38 -0400

    add adafruit io

---
+++ adafruitio_color.py
@@ -0,0 +1,80 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+# adafruit_circuitpython_adafruitio usage with an esp32spi_socket
+from random import randint
+import board
+import busio
+from digitalio import DigitalInOut
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+from adafruit_esp32spi import adafruit_esp32spi
+import adafruit_requests as requests
+from adafruit_io.adafruit_io import IO_HTTP, AdafruitIO_RequestError
+import neopixel
+import time
+
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+
+# Add a secrets.py to your filesystem that has a dictionary called secrets with "ssid" and
+# "password" keys with your WiFi credentials. DO NOT share that file or commit it into Git or other
+# source control.
+# pylint: disable=no-name-in-module,wrong-import-order
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# If you are using a board with pre-defined ESP32 Pins:
+esp32_cs = DigitalInOut(board.ESP_CS)
+esp32_ready = DigitalInOut(board.ESP_BUSY)
+esp32_reset = DigitalInOut(board.ESP_RESET)
+
+# If you have an externally connected ESP32:
+# esp32_cs = DigitalInOut(board.D9)
+# esp32_ready = DigitalInOut(board.D10)
+# esp32_reset = DigitalInOut(board.D5)
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+print("Connecting to AP...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets["ssid"], secrets["password"])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+print("Connected to", str(esp.ssid, "utf-8"), "\tRSSI:", esp.rssi)
+
+socket.set_interface(esp)
+requests.set_socket(socket, esp)
+
+# Set your Adafruit IO Username and Key in secrets.py
+# (visit io.adafruit.com if you need to create an account,
+# or if you need your Adafruit IO key.)
+aio_username = secrets["aio_username"]
+aio_key = secrets["aio_key"]
+
+# Initialize an Adafruit IO HTTP API object
+io = IO_HTTP(aio_username, aio_key, requests)
+
+# Get the 'temperature' feed from Adafruit IO
+color_feed = io.get_feed("color")
+
+prev_color = "#000000"
+
+# Send random integer values to the feed
+while True:
+    received_data = io.receive_data(color_feed["key"])["value"]
+    if received_data != prev_color:
+        print(f"Got new color {received_data}")
+        prev_color = received_data
+        pixel.fill(hex_to_rgb(received_data))
+        
+    time.sleep(2)
